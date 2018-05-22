2a75a72295e05777a92b1fb46bfa445afde7a86b
ivethlopez <iaguilarlopez400@gmail.com>
2018-05-22 08:54:15 -0500

    2 subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,30 +1,26 @@
 // JavaScript Document 
 
-$(document).ready(function (e) {
-   document .addEventListener ("deviceready",function() {
+  $(document).ready(function (e) {
+     document.addEventListener("deviceready",function(){
 
-   
-    $('#izquierda').on("swipeleft",function(){
-       navigator.notification. alert("Deslizo a la izquierda",function(){"Aplicacion 7","Aceptar"});
+          $('#izquierda').on("swipeleft",function() {
+               navigator.notification. alert("Deslizo a la izquierda",function(){"Aplicacion7","Aceptar"});
 });
 
-$('#derecha').on("swiperight",function(){
-    navigator.notification. confirm("¿Que quieres hacer?",function(op){
-       switch(op)
-         {
-             case 1:
-             navigator.notification.beep(1);
-             break;
-
-             case 2:
-             navigator.notification.vibrate(1000);
-             break;
-            }
-           },"Aplicacion7","Beep", Vibrar, Cancelar");
+$('#derecha').on("swiperight",function() {
+               navigator.notification. alert("¿Que quieres hacer?",function(op){
+                    switch(op)
+                     {
+                       case 1:
+                       navigator.notification.beep(1);
+                       break;
+                     
+                       case 2:
+                       navigator.notification.vibrate(1000);
+                       break;
+                     }
+                  },"Aplicacion7","Beep, Vibrar, Cancelar");
 });
-   },false);
-}); 
+    },false);
+});
 
-
-
-  
